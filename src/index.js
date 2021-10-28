abed103e2fdebb3a0203a29feb3541759f48d10b
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-10-28 15:05:14 +0300

    changed to 4 in arow

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 "use strict";
 
+class Model{
+    constructor(){
+        
+    }
+}
+class View{
+    constructor(){
 
+        
+    }
+    
+    // createElement(tagName, classes = [], attributes = {}, eventListeners = {}) {
+    //     const el = document.createElement(tagName);
+    //     // Classes
+    //     for(const cls of classes) {
+    //       el.classList.add(cls);
+    //     }
+    //     // Attributes
+    //     for (const attr in attributes) {
+    //       el.setAttribute(attr, attributes[attr]);
+    //     }
+    //     // Event Listeners
+    //     for (const listener in eventListeners) {
+    //         el.addEventListener(listener, eventListeners[listener]);
+    //       }
+    //     return el;
+    // }
+    
+    getElement(selector) {
+        const element = document.querySelector(selector);
+        return element;
+      }
+}
+class Controller{
+    constructor(){
+        
+    }
+}
